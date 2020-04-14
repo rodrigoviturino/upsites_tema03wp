cd9d76ffa849a50d702b1a5eac01d6a990f0a826
Rodrigo Viturino <Rodrigo@upsites.digital>
2020-04-14 16:34:51 -0300

    Header - Menu sticky ajuste no padding

--- header.php
+++ header.php
@@ -4,8 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <!-- <link rel="stylesheet" href="./theme.min.css">
-    <link rel="stylesheet" href="./fonts/fonts.css"> -->
     <title>Theme 03</title>
 
     <?php wp_head(); ?>
